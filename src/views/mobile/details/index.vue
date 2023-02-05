007fb9f834c4c04011d479fc664bd396cd6df6bd
wzx2206221617 <2206221617@qq.com>
2023-02-05 17:18:22 +0800

    fix；详情页更新

--- src/views/mobile/details/index.vue
+++ src/views/mobile/details/index.vue
@@ -9,7 +9,18 @@ import image1 from '@/assets/image/1.jpg'
 const route = useRoute()
 const bannerList = ref<IGoodItem>([])
 const ActionImage = ref<number>()
+const goodSizeIndex = ref<number>()
+const SizeNumber = ref<number>()
+
 const carousel = ref()
+const goodSize = ref([
+    { label: 39, value: 1, disabled: false, num: 10 },
+    { label: 40, value: 2, disabled: true, num: 11 },
+    { label: 41, value: 3, disabled: false, num: 21 },
+    { label: 42, value: 4, disabled: true, num: 134 },
+    { label: 43, value: 5, disabled: true, num: 10 },
+    { label: 44, value: 6, disabled: false, num: 10 },
+])
 const getInfo = (): void => {
     bannerList.value = [
         { id: 22, img: kuzi, good_name: 'Nike Solo Swoosh', title: "男子加绒套头连帽衫", variety: 12, reality_price: 500, discounts_price: 199 },
@@ -28,7 +39,11 @@ const changeImage = (v: number) => {
         carousel.value.setActiveItem('i' + v)
     })
 }
-
+const changeSizeItem = (item: any, index: number) => {
+    console.log(item);
+    goodSizeIndex.value = index
+    SizeNumber.value = item.num
+}
 console.log(route.query);
 getInfo()
 </script>
@@ -51,6 +66,24 @@ getInfo()
             <div v-for="(item, index) in bannerList" :key="index" @click="changeImage(item.id)">
                 <el-image :class="[ActionImage === item.id ? 'showImgAction' : 'showImg']" :src="item.img" fit="fill" />
             </div>
+        </div>
+        <div>
+            <div class="transparent">
+                <p>选择尺码<span v-if="SizeNumber" style="margin-left: 10px;">库存:{{ SizeNumber }}</span></p>
+                <p>尺码表</p>
+            </div>
+            <div class="hzulvp">
+                <div v-for="(item, index) in goodSize" :key="index">
+                    <div :class="[item.disabled ? 'disabledButGod' : goodSizeIndex === index ? 'activeButGod' : 'butGod']"
+                        @click="changeSizeItem(item, index)">{{
+    item.label
+                        }}</div>
+                </div>
+            </div>
+        </div>
+        <div class="titleName">产品介绍</div>
+        <div class="textMain">
+            穿上温暖多绒的 Jordan Essential Winter 男子加绒连帽衫，让你轻松应对冷天。刺绣 AIR JORDAN 字样，打造舒适穿着体验，又不失潮酷感。
         </div>
     </div>
 </template>
@@ -88,7 +121,8 @@ getInfo()
     grid-template-columns: repeat(auto-fill, 175px);
     column-gap: 10px;
     row-gap: 5px;
-    justify-content:center;
+    justify-content: center;
+
     .showImg {
         border-radius: 5px;
         width: 175px;
@@ -101,5 +135,64 @@ getInfo()
         width: 175px;
         height: 175px;
     }
+}
+
+.transparent {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 20px;
+}
+
+:deep(.hzulvp) {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 140px);
+    column-gap: 5px;
+    row-gap: 5px;
+    justify-content: center;
+
+    .butGod {
+        width: 140px;
+        height: 80px;
+        border: 1px solid #E5E5E5;
+        border-radius: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #FFFFFF;
+    }
+
+    .disabledButGod {
+        width: 140px;
+        height: 80px;
+        pointer-events: none;
+        cursor: default;
+        color: rgb(221, 221, 221);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #FFFFFF;
+        border: none;
+    }
+
+    .activeButGod {
+        width: 140px;
+        height: 80px;
+        border: 1px solid black;
+        border-radius: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #FFFFFF;
+    }
+
+
+}
+.titleName{
+    font-size: 20px;
+    font-weight: 500;
+    padding: 20px;
+}
+.textMain {
+    padding: 20px;
 }
 </style>
